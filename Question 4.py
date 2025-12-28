26e2fdaae71640daa4a12ba088ebec0fb70365ba
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 17:59:26 +0530

    Create Question 4.py

---
+++ Question 4.py
@@ -0,0 +1,17 @@
+'''
+Count Odd 3 Digit Numbers (Ignore None)
+Write a function count_odd_three_digit_nums(nums) that takes a list of integers nums and returns the count of numbers that are:
+
+Odd.
+Three-digit numbers (ignoring the negative sign if present).
+Not None.
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Examples:
+count_odd_three_digit_nums([101, -203, None, 99, 300]) → 2
+Explanation: 101 and -203 are odd three-digit numbers, ignoring None.
+count_odd_three_digit_nums([None, 120, 301, -401, 78]) → 2
+Explanation: 301 and -401 qualify as odd three-digit numbers.
+count_odd_three_digit_nums([10, 305, 507, 99]) → 2
+Explanation: 305 and 507 qualify as odd three-digit numbers.
+'''
