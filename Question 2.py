7c6f2c80a9e6b2611ea0ed3ef53ec268bcc9ff65
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 17:56:53 +0530

    Create Question 2.py

---
+++ Question 2.py
@@ -0,0 +1,18 @@
+'''
+Check If the given domain is .com or .in
+Write a function is_dot_com_or_dot_in(domain) that takes a string domain and checks whether it ends with .com or .in.
+The function should return True if the domain has the specified suffix; otherwise, it should return False.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Examples
+
+is_dot_com_or_dot_in("example.com") -> True
+Explanation: The domain ends with .com.
+is_dot_com_or_dot_in("website.in") -> True
+Explanation: The domain ends with .in.
+is_dot_com_or_dot_in("mydomain.org") -> False
+Explanation: The domain does not end with .com or .in.
+is_dot_com_or_dot_in("invalidcom") -> False
+Explanation: The domain does not end with .com or .in.
+'''
