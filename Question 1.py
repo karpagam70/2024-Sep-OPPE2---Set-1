dd6eaeabb20de9f0823b1afe460df260cb944ab8
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 17:56:03 +0530

    Create Question 1.py

---
+++ Question 1.py
@@ -0,0 +1,16 @@
+'''
+Check If Even Two-Digit Number
+Write a function is_even_two_digit_number that checks whether a given number is an even two-digit number.
+
+A number is considered a two-digit number if it has exactly two digits excluding any negative sign.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+For example:
+
+24 is an even two-digit number.
+-36 is an even two-digit number.
+5 is not a two-digit number.
+101 is not a two-digit number.
+-19 is not an even number.
+'''
